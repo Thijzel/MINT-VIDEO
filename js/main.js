4af358484ced7bc3686b88a1ee291314bc252a04
Joep Meindertsma <joepmeindertsma@gmail.com>
2018-09-16 16:07:33 +0200

    Various improvements

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var menu = document.querySelector("#nav");
+var menu = document.body;
+
+function openMenu () {
+  menu.classList.add('navbar--open');
+}
+
+function closeMenu () {
+  menu.classList.remove('navbar--open');
+}
